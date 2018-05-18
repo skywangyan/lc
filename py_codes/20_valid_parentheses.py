324022b80f006b9e9c59c49f80d52bfca731def8
ywang1445 <ywang1445@bloomberg.net>
2018-05-17 23:32:45 -0400

    valid parentheses

---
+++ py_codes/20_valid_parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for char in s:
+            if char == '{' or char == '[' or char == "(":
+                stack.append(char)
+            elif char == '}':
+                if not stack or stack.pop() != '{':
+                    return False
+            elif char == ']':
+                if not stack or stack.pop() != '[':
+                    return False
+            elif char == ')'
+                if not stack or stack.pop() != '(':
+                    return False
+        if not stack:
+            return True
+        else:
+            return False
+            
