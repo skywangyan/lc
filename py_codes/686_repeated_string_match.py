5adf09dacaf523d760e1494d3482c0663477908c
jinshiweikai <jinshiweikai@gmail.com>
2018-08-11 18:54:38 -0700

    keep moving

---
+++ py_codes/686_repeated_string_match.py
@@ -0,0 +1,25 @@
+# Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+# For example, with A = "abcd" and B = "cdabcdab".
+# Note:
+# The length of A and B will be between 1 and 10000.
+import math
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: int
+        """
+        lentha, lenthb = len(A), len(B)
+        times = int(math.ceil(lenthb / lentha))
+        combined = A * times
+        i = 0
+        if B in combined:
+            return times
+        if B in combined+A:
+            return times + 1
+        if B in combined+A+A:
+            return times + 2
+        return -1
+s = Solution()
+print s.repeatedStringMatch("abcd","cdabcdab")
---
+++ py_codes/766_Teoplitz_matrix.py
@@ -0,0 +1,71 @@
+# A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+# Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+
+# Example 1:
+
+# Input:
+# matrix = [
+#   [1,2,3,4],
+#   [5,1,2,3],
+#   [9,5,1,2]
+# ]
+# Output: True
+# Explanation:
+# In the above grid, the diagonals are:
+# "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+# In each diagonal all elements are the same, so the answer is True.
+# Example 2:
+
+# Input:
+# matrix = [
+#   [1,2],
+#   [2,2]
+# ]
+# Output: False
+# Explanation:
+# The diagonal "[1, 2]" has different elements.
+
+# Note:
+
+# matrix will be a 2D array of integers.
+# matrix will have a number of rows and columns in range [1, 20].
+# matrix[i][j] will be integers in range [0, 99].
+
+# Follow up:
+
+# What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+# What if the matrix is so large that you can only load up a partial row into the memory at once?
+
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        width, height = len(matrix[0]), len(matrix)
+        for i in xrange(width):
+            target = matrix[0][i]
+            x, y = 0, i
+            while x + 1 < height and y + 1 < width:
+                x += 1
+                y += 1
+                if matrix[x][y] != target:
+                    return False
+        for j in xrange(height):
+            target = matrix[j][0]
+            x,y = j,0
+            while x + 1 < height and y + 1 < width:
+                x += 1
+                y += 1
+                if matrix[x][y] != target:
+                    return False
+        return True
+
+s = Solution()
+print s.isToeplitzMatrix( [
+  [1,2,3,4],
+  [5,1,2,3],
+  [9,5,1,2]
+])
