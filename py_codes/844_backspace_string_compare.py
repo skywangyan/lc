ed9b5ff2cdb5cdbfde7fa3d78f28b7e3ad646085
jinshiweikai <jinshiweikai@gmail.com>
2018-10-08 22:47:07 -0700

    844

---
+++ py_codes/844_backspace_string_compare.py
@@ -0,0 +1,54 @@
+# Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+# Example 1:
+
+# Input: S = "ab#c", T = "ad#c"
+# Output: true
+# Explanation: Both S and T become "ac".
+# Example 2:
+
+# Input: S = "ab##", T = "c#d#"
+# Output: true
+# Explanation: Both S and T become "".
+# Example 3:
+
+# Input: S = "a##c", T = "#a#c"
+# Output: true
+# Explanation: Both S and T become "c".
+# Example 4:
+
+# Input: S = "a#c", T = "b"
+# Output: false
+# Explanation: S becomes "c" while T becomes "b".
+# Note:
+
+# 1 <= S.length <= 200
+# 1 <= T.length <= 200
+# S and T only contain lowercase letters and '#' characters.
+# Follow up:
+
+# Can you solve it in O(N) time and O(1) space?
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        s1,s2 = [], []
+        for c in S:
+            if c == '#' and s1:
+                s1.pop()
+            elif c != '#':
+                s1.append(c)
+        for char in T:
+            if char == '#' and s2:
+                s2.pop()
+            elif char != '#':
+                s2.append(char)
+
+        return s1 == s2
+
+s = Solution()
+
+print s.backspaceCompare("y#fo##f", "y#f#o##f")
