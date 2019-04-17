4ab4c2de19d1e663bdb8885e99a20eb8a2c4cf70
yanalienware <jingchengsuozhi.wy@gmail.com>
2019-04-16 22:40:50 -0700

    add description

--- py_codes/31_next_permutation.py
+++ py_codes/31_next_permutation.py
@@ -1,3 +1,16 @@
+'''
+Implement next permutation, which rearranges numbers into the lexicographically next
+ greater permutation of numbers.
+If such arrangement is not possible, it must rearrange it as the lowest possible order
+ (ie, sorted in ascending order).
+The replacement must be in-place and use only constant extra memory.
+Here are some examples. Inputs are in the left-hand column and its corresponding 
+outputs are in the right-hand column.
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+'''
+
 class Solution(object):
     def nextPermutation(self, nums):
         """
@@ -21,6 +34,6 @@ class Solution(object):
         nums.reverse()
                 
 s = Solution()
-a = [1,5,1]
+a = [1,2]
 s.nextPermutation(a)
 print a
--- py_codes/44_wildcard_matching.py
+++ py_codes/44_wildcard_matching.py
@@ -1,3 +1,51 @@
+'''
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with 
+support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+
+Example 1:
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+
+Example 3:
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+Example 4:
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the 
+substring "dce".
+
+Example 5:
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false
+'''
 import argparse
 class Solution(object):
     def isMatch(self, s, p):
@@ -32,34 +80,9 @@ class Solution(object):
                         matrix[i][j] = 0
                 else:
                     matrix[i][j] = 0
-        return matrix[m][n]
+        return True if matrix[m][n] else False
 
-    def isMatch2(self, s, p):
-        s_cur = 0;
-        p_cur= 0;
-        match = 0;
-        star = -1;
-        while s_cur<len(s):
-            if p_cur< len(p) and (s[s_cur]==p[p_cur] or p[p_cur]=='?'):
-                s_cur = s_cur + 1
-                p_cur = p_cur + 1
-            elif p_cur<len(p) and p[p_cur]=='*':
-                match = s_cur
-                star = p_cur
-                p_cur = p_cur+1
-            elif (star!=-1):
-                p_cur = star+1
-                match = match+1
-                s_cur = match
-            else:
-                return False
-        while p_cur<len(p) and p[p_cur]=='*':
-            p_cur = p_cur+1
-             
-        if p_cur==len(p):
-            return True
-        else:
-            return False
+    
 
 
 def main():
@@ -68,7 +91,7 @@ def main():
     parser.add_argument("p")
     args = parser.parse_args()
     s = Solution()
-    print s.isMatch2(args.s, args.p)
+    print s.isMatch(args.s, args.p)
 
 if __name__ == "__main__":
     main()
