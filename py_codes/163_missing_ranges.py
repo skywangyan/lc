f82fe3e0970258ac6e278d0403ba40a26182f092
yanalienware <jingchengsuozhi.wy@gmail.com>
2019-05-02 23:19:12 -0700

    166

--- py_codes/163_missing_ranges.py
+++ py_codes/163_missing_ranges.py
@@ -1,3 +1,9 @@
+'''
+"""
+Given a sorted integer array where the range of elements are [lower, upper] inclusive, return its missing ranges.
+For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4->49", "51->74", "76->99"].
+"""
+'''
 class Solution(object):
     def findMissingRanges(self, nums, lower, upper):
         """
@@ -24,6 +30,19 @@ class Solution(object):
             res.append(str(gapl))
         return res
 
-
+    def findMissingRanges2(self, nums, lower, upper):
+        left = lower
+        res = []
+        for num in nums:
+            if left < num:
+                right = num - 1
+                res.append("{0}->{1}".format(left,right) if left < right else "{0}".format(left))
+                left = num + 1
+            elif left == num:
+                left += 1
+        else:
+            if left <= upper:
+                res.append("{0}->{1}".format(left,upper) if left < upper else "{0}".format(left))
+        return res
 s = Solution()
-print s.findMissingRanges([1,1,1],1,1)
+print s.findMissingRanges2([0, 1, 3, 50, 75],0,75)
