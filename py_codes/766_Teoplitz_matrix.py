6e2bed4b8fe1fb354f19f2de065a3e0a6f13c72c
yanalienware <jingchengsuozhi.wy@gmail.com>
2019-05-24 21:45:35 -0700

    766

--- py_codes/766_Teoplitz_matrix.py
+++ py_codes/766_Teoplitz_matrix.py
@@ -35,8 +35,10 @@
 
 # Follow up:
 
-# What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
-# What if the matrix is so large that you can only load up a partial row into the memory at once?
+# What if the matrix is stored on disk, and the memory is limited such that you can only 
+# load at most one row of the matrix into the memory at once?
+# What if the matrix is so large that you can only load up a partial row into the memory at 
+# once?
 
 class Solution(object):
     def isToeplitzMatrix(self, matrix):
@@ -63,8 +65,31 @@ class Solution(object):
                     return False
         return True
 
+    def isToeplitzMatrix2(self, matrix):
+        if not matrix:
+            return  True
+        width, height = len(matrix[0]), len(matrix)
+        for i in xrange(width):
+            start_value = matrix[0][i]
+            m, n = 0, i
+            while m < height and n < width:
+                if matrix[m][n] != start_value:
+                    return False
+                m += 1
+                n +=1
+        for j in xrange(height):
+            start_value = matrix[j][0]
+            m, n = j, 0
+            while m < height and n < width:
+                if matrix[m][n] != start_value:
+                    return False
+                m, n = m+1, n+1
+        return True
+
+
+
 s = Solution()
-print s.isToeplitzMatrix( [
+print s.isToeplitzMatrix2( [
   [1,2,3,4],
   [5,1,2,3],
   [9,5,1,2]
