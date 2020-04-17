f017559dca8aaebf4aca93f7081cac31f7ecaeeb
ywang1445 <ywang1445@bloomberg.net>
2020-04-17 00:11:25 -0700

    next permutation & junit
    Signed-off-by: ywang1445 <ywang1445@bloomberg.net>

---
+++ javaCodes/src/main/java/edu/nju/ee/zerosix/yan/NextPermutation.java
@@ -0,0 +1,67 @@
+package edu.nju.ee.zerosix.yan;
+
+import java.util.ArrayList;
+
+/*
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+        If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+        The replacement must be in-place and use only constant extra memory.
+
+        Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+        1,2,3 → 1,3,2
+        3,2,1 → 1,2,3
+        1,1,5 → 1,5,1
+
+*/
+public class NextPermutation implements Solution {
+    @Override
+    public void run() {
+        int[] input = {1,3,2};
+        nextPermutation(input);
+        for(int i: input) {
+            System.out.println(i);
+        }
+
+    }
+
+    public void nextPermutation(int[] nums) {
+        int arrayLen = nums.length;
+        for(int i = arrayLen-1; i > 0; i--){
+            if(nums[i-1] < nums[i]) {
+                int tmp = nums[i-1];
+                int j = arrayLen - 1;
+                while(j >= 0) {
+                    if (nums[j] > tmp) {
+                        nums[i-1] = nums[j];
+                        nums[j] = tmp;
+                        break;
+                    }
+                    j--;
+                }
+                int left = i;
+                int right = arrayLen - 1;
+                while(left < right){
+                    tmp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = tmp;
+                    left++;
+                    right--;
+                }
+                return;
+            }
+        }
+        int left = 0;
+        int right = arrayLen - 1;
+        int tmp = 0;
+        while(left < right){
+            tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+}
---
+++ javaCodes/src/test/java/edu/nju/ee/zerosix/yan/LeetCodeTest.java
@@ -0,0 +1,11 @@
+package edu.nju.ee.zerosix.yan;
+
+import org.junit.Test;
+
+public class LeetCodeTest {
+    @Test
+    public void nextPermutationTest(){
+        NextPermutation np = new NextPermutation();
+        np.run();
+    }
+}
